9cfda756f1c6171c1b5090eedb67f9d53b14db96
pablopussell <pp085ster@gmail.com>
2015-11-27 09:06:23 +0100

    Extra-class 26th Nov

--- src/PokerTest.java
+++ src/PokerTest.java
@@ -7,28 +7,35 @@ public class PokerTest {
 	@Test
 	public void IsFourOfAKindWithOnes() {
 		
-		Hand hand = new Hand("1♦ 1♥ 1♠ 1♣ 10♥");
+		Hand hand = new Hand("1♣ 1♥ 1♠ 1♦ 10♦");
 		assertEquals("Four Of A Kind", hand.type());
 	}
 	
 	@Test
 	public void IsFourOfAKindWithFives() {
 		
-		Hand hand = new Hand("5♦ 5♥ 5♠ 5♣ 10♥");
+		Hand hand = new Hand("5♣ 5♥ 5♠ 5♦ 10♦");
 		assertEquals("Four Of A Kind", hand.type());
 	}
 	
 	@Test
-	public void IsFourOfAKindWithTwos() {
+	public void IsThreeOfAKindWithTwos() {
 		
-		Hand hand = new Hand("2♦ 2♥ 2♠ 2♣ 10♥");
-		assertEquals("Four Of A Kind", hand.type());
-	}
-
-	@Test
-	public void IsThreeOfAKindWithOnlyNumbers() {
-		
-		Hand hand = new Hand("4♦ 4♥ 4♠ 6♣ 10♥");
+		Hand hand = new Hand("2♣ 2♥ 2♠ 4♦ 10♠");
 		assertEquals("Three Of A Kind", hand.type());
+	}
+	
+	@Test
+	public void IsOnePair() {
+		
+		Hand hand = new Hand("3♣ 3♥ 5♥ 2♦ 10♠");
+		assertEquals("One Pair", hand.type());
+	}
+	
+	@Test
+	public void IsFlush() {
+		
+		Hand hand = new Hand("2♣ 5♣ 8♣ 9♣ 10♣");
+		assertEquals("Flush", hand.type());
 	}
 }
